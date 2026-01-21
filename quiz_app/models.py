e0c562b01cfe817737f1d94f7070f34fa3bccac4
LarsThoennes <larsthoennes@gmail.com>
2026-01-21 21:08:55 +0100

    feat(quiz): introduce quiz models
    - Added Quiz model with metadata fields
    - Added QuizQuestion model with quiz relation
    - Added QuizOption model for question options

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Quiz(models.Model):  
+  title = models.CharField(max_length=100)  
+  description = models.TextField()  
+  video_url = models.CharField(max_length=255)  
+
+  created_at = models.DateTimeField(auto_now_add=True)  
+  updated_at = models.DateTimeField(auto_now_add=True)  
+
+  def __str__(self):
+        return self.title
+class QuizQuestion(models.Model):  
+  quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='questions')  
+  question_title = models.CharField(max_length=255)  
+  answer = models.CharField(max_length=255)  
+
+  created_at = models.DateTimeField(auto_now_add=True)  
+  updated_at = models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+        return self.question_title
+class QuizOption(models.Model):
+    question = models.ForeignKey(
+        QuizQuestion,
+        on_delete=models.CASCADE,
+        related_name='question_options'
+    )
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
