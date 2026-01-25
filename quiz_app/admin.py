45a1e672a41a4a786cbbf7eaab64fb1adf8bfee5
LarsThoennes <larsthoennes@gmail.com>
2026-01-25 15:41:38 +0100

    chore(quiz): include creator field in admin configuration
    - Added creator to list_display and list_filter in Quiz admin
    - Improved visibility of quiz ownership in admin panel

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Quiz, QuizQuestion, QuizOption
 
 class QuizExtension(admin.ModelAdmin):
-    list_filter= ['id','title', 'video_url', 'video_id', 'created_at', 'updated_at']
-    list_display = ('id','title', 'video_url', 'video_id')
+    list_filter= ['id','creator','title', 'video_url', 'video_id', 'created_at', 'updated_at']
+    list_display = ('id','creator','title', 'video_url', 'video_id')
     readonly_fields = ['id']
 
 class QuizQuestionExtension(admin.ModelAdmin):
