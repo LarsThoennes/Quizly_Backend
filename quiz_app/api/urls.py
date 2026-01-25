3cff008d8fc217dfb59368395a5dda8dd1cc9019
LarsThoennes <larsthoennes@gmail.com>
2026-01-25 18:02:08 +0100

    chore(quiz): add quizzes list API route
    - Added quizzes endpoint for retrieving user quizzes
    - Registered QuizListView in api urls

--- quiz_app/api/urls.py
+++ quiz_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateQuizView
+from .views import CreateQuizView, QuizListView
 
 urlpatterns = [
     path('createQuiz/', CreateQuizView.as_view(), name='create_quiz'),
+    path('quizzes/', QuizListView.as_view(), name='quiz_list'),
 ]
