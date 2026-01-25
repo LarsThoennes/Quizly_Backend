2b06d2f382cbe0a77fe1af944aac91d288d35f73
LarsThoennes <larsthoennes@gmail.com>
2026-01-25 14:51:30 +0100

    chore(quiz): extend admin configuration for quiz models
    - Added custom ModelAdmin classes for Quiz, QuizQuestion and QuizOption
    - Configured list_display and list_filter for better overview
    - Set readonly fields for id and related fields

--- quiz_app/admin.py
+++ quiz_app/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Quiz, QuizQuestion, QuizOption
 
-admin.site.register(Quiz)
-admin.site.register(QuizQuestion)
-admin.site.register(QuizOption)
+class QuizExtension(admin.ModelAdmin):
+    list_filter= ['id','title', 'video_url', 'video_id', 'created_at', 'updated_at']
+    list_display = ('id','title', 'video_url', 'video_id')
+    readonly_fields = ['id']
+
+class QuizQuestionExtension(admin.ModelAdmin):
+    list_filter= ['id','quiz','question_title','created_at', 'updated_at']
+    list_display = ('id','quiz','question_title', 'answer')
+    readonly_fields = ['id','quiz']
+
+class QuizOptionExtension(admin.ModelAdmin):
+    list_filter= ['id','question','text']
+    list_display = ('id','question','text')
+    readonly_fields = ['id','question']
+
+admin.site.register(Quiz, QuizExtension)
+admin.site.register(QuizQuestion, QuizQuestionExtension)
+admin.site.register(QuizOption, QuizOptionExtension)
