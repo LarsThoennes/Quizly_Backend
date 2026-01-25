f7945aa1b392a3f4e3d653eebdfa83509bc50f80
LarsThoennes <larsthoennes@gmail.com>
2026-01-25 14:49:31 +0100

    style(quiz): code formatted

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -4,12 +4,14 @@ class Quiz(models.Model):
   title = models.CharField(max_length=100)  
   description = models.TextField()  
   video_url = models.CharField(max_length=255)  
+  video_id = models.CharField(max_length=50, null=True, blank=True)
 
   created_at = models.DateTimeField(auto_now_add=True)  
   updated_at = models.DateTimeField(auto_now_add=True)  
 
   def __str__(self):
         return self.title
+
 class QuizQuestion(models.Model):  
   quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='questions')  
   question_title = models.CharField(max_length=255)  
@@ -20,12 +22,9 @@ class QuizQuestion(models.Model):
 
   def __str__(self):
         return self.question_title
+
 class QuizOption(models.Model):
-    question = models.ForeignKey(
-        QuizQuestion,
-        on_delete=models.CASCADE,
-        related_name='question_options'
-    )
+    question = models.ForeignKey(QuizQuestion, on_delete=models.CASCADE, related_name='question_options')
     text = models.CharField(max_length=255)
 
     def __str__(self):
