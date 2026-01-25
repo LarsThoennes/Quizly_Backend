8e046297389576e7fe0c3d3af5c132193e31d660
LarsThoennes <larsthoennes@gmail.com>
2026-01-25 18:33:01 +0100

    chore(quiz): add quiz detail view with update and delete support
    - Added QuizDetailView for retrieving single quizzes
    - Implemented permission checks for quiz ownership
    - Added PATCH method to update quiz title
    - Added DELETE method to remove quizzes

--- quiz_app/api/views.py
+++ quiz_app/api/views.py
@@ -2,8 +2,10 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import QuizCreateSerializer, QuizDetailSerializer
+from .serializers import QuizCreateSerializer, QuizDetailSerializer, QuizUpdateSerializer
 from ..models import Quiz
+from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import PermissionDenied
 
 class CreateQuizView(APIView):
     permission_classes = [IsAuthenticated]
@@ -30,3 +32,51 @@ class QuizListView(APIView):
             serializer.data,
             status=status.HTTP_200_OK
         )
+    
+class QuizDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, pk):
+        quiz = get_object_or_404(Quiz, pk=pk)
+
+        if quiz.creator != request.user:
+            raise PermissionDenied("You do not have permission to access this quiz.")
+
+        serializer = QuizDetailSerializer(
+            quiz,
+            context={"remove_timestamps": True}
+        )
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, pk):
+        quiz = get_object_or_404(Quiz, pk=pk)
+
+        if quiz.creator != request.user:
+            raise PermissionDenied("You do not have permission to delete this quiz.")
+        
+        quiz.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def patch(self, request, pk):
+        quiz = get_object_or_404(Quiz, pk=pk)
+
+        if quiz.creator != request.user:
+            raise PermissionDenied("You do not have permission to update this quiz.")
+
+        serializer = QuizUpdateSerializer(
+            quiz,
+            data=request.data,
+            partial=True
+        )
+
+        serializer.is_valid(raise_exception=True)
+        quiz = serializer.save()
+
+        detail_serializer = QuizDetailSerializer(
+            quiz,
+            context={"remove_timestamps": True}
+        )
+
+        return Response(detail_serializer.data, status=status.HTTP_200_OK)
