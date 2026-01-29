69d6294bf092cceec845508342dad1268b93198f
LarsThoennes <larsthoennes@gmail.com>
2026-01-29 11:04:32 +0100

    chore(quiz): allow updating quiz description
    - Extended QuizUpdateSerializer to support updating the description field
    - Updated update logic to save title and description changes

--- quiz_app/api/serializers.py
+++ quiz_app/api/serializers.py
@@ -148,9 +148,11 @@ class QuizUpdateSerializer(serializers.ModelSerializer):
             model = Quiz
             fields = [
                 "title",
+                "description",
             ]
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get("title", instance.title)
+        instance.description = validated_data.get("description", instance.description)
         instance.save()
         return instance
